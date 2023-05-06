81435acd164903e309cd55d4455e99785bc76d85
alex28042 <alex28042@gmail.com>
2023-05-06 17:15:07 +0200

    Kafka producer

--- src/main/java/com/example/Urls/Application/UrlService.java
+++ src/main/java/com/example/Urls/Application/UrlService.java
@@ -2,6 +2,7 @@ package com.example.Urls.Application;
 
 import com.example.Urls.Domain.Url;
 import com.example.Urls.Domain.UrlRepository;
+import com.example.Urls.Infrastructure.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,14 @@ public class UrlService {
     @Autowired
     private UrlRepository urlRepository;
 
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
     public Url save(Url url) {
-        return urlRepository.save(url);
+        Url urlCreated = urlRepository.save(url);
+
+        kafkaProducer.send("test", urlCreated);
+
+        return urlCreated;
     }
 }
--- src/main/java/com/example/Urls/Infrastructure/KafkaProducer.java
+++ src/main/java/com/example/Urls/Infrastructure/KafkaProducer.java
@@ -1,6 +1,7 @@
 package com.example.Urls.Infrastructure;
 
 
+import com.example.Urls.Domain.Url;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, Url> kafkaTemplate;
 
-    public void send(String topic, String message) {
+    public void send(String topic, Url message) {
         kafkaTemplate.send(topic, message);
     }
 }
